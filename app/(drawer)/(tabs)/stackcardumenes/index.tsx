c533388728fb629fd49cd55e5fe912d8d241ce2c
Rugroso <133253941+Rugroso@users.noreply.github.com>
2025-05-24 22:03:42 -0700

    solved ui problems in cardumenes

--- app/(drawer)/(tabs)/stackcardumenes/index.tsx
+++ app/(drawer)/(tabs)/stackcardumenes/index.tsx
@@ -1301,7 +1301,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingTop: 100,
     paddingHorizontal: 32,
   },
   emptyText: {
